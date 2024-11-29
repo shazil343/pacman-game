00d3c89e68fef2818934ec356368b7a8ac64c489
Shazil Khan <shazil.khan@ucalgary.ca>
2024-11-29 15:07:30 -0700

    maze push

--- src/main/java/com/example/groupproject16/Maze.java
+++ src/main/java/com/example/groupproject16/Maze.java
@@ -14,7 +14,8 @@ public class Maze extends Application {
 
 
     private static final int TILE_SIZE = 10;
-    private static final int ROWS = 65; // Number of rows in the maze
+    private static final int ROWS = 80; // Number of rows in the maze
+    private static final int BLUEROWS =60;
     private static final int COLUMNS = 54;// Number of columns in the maze
 
 
@@ -27,7 +28,7 @@ public class Maze extends Application {
             for (int row = 8; row < ROWS; row++) {
                 for (int col = 0; col < COLUMNS; col++) {
                     // Set border tiles to 1 (wall) and inner tiles to 0 (path)
-                    if (row == 8 || row == ROWS - 1 || col == 0 || col == COLUMNS - 1) {
+                    if (row == 8 || row == ROWS - 20 || col == 0 || col == COLUMNS - 1) {
                         mazeLayout[row][col] = 1; // Wall
                     } else {
                         mazeLayout[row][col] = 0; // Path
@@ -42,7 +43,7 @@ public class Maze extends Application {
             for (int row = 0; row < ROWS; row++) {
                 for (int col = 0; col < COLUMNS; col++) {
                     Rectangle tile = new Rectangle(TILE_SIZE, TILE_SIZE);
-                    if (mazeLayout[row][col] == 1) {
+                    if (mazeLayout[row][col] == 1 && row<BLUEROWS+1) {
                         tile.setFill(Color.BLUE); // Wall
                     } else {
                         tile.setFill(Color.BLACK); // Path
